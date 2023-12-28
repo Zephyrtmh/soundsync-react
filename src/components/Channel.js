6b9a2b41c2443eb33fce59e1d40261f6ead321d4
Zephyr <Zephyrtmh@gmail.com>
2023-12-29 03:17:41 +0800

    added styling

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Chat from "./Chat";
-import "./Chat.css";
+import "./Channel.css";
 
 function Channel() {
+  const [showChat, setShowChat] = useState(false);
+
+  const handleKeyDown = () => {
+    setShowChat(!showChat);
+  };
+
+  // Attach the event listener when the component mounts
+  document.addEventListener("keydown", handleKeyDown);
+
   return (
-    <div className="channel-container">
-      <div>test</div>
-      <Chat></Chat>
+    <div className="channel-display-container">
+      <div className="channel-navigation-bar">channels</div>
+      <div className="channel">
+        <div>live stream</div>
+        <div className="chat">
+          <Chat></Chat>
+        </div>
+      </div>
     </div>
   );
 }
